5796904f82c4e4eb5bd58724499df4082ae3c929
Guido <guido.tag@gmail.com>
2015-10-08 19:00:09 -0300

    Unifique instrucciones DML y DDL en un solo archivo.

--- TP/codigo/tablas_con_defines.sql
+++ TP/codigo/tablas_con_defines.sql
@@ -1,11 +1,12 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
+
 CREATE TABLE provincia (
  idProvincia INTEGER NOT NULL,
  nombre VARCHAR(255) NOT NULL,
  PRIMARY KEY(idProvincia)
 );
-INSERT INTO "provincia" VALUES(0,'Buenos Aires');
+INSERT INTO "provincia" VALUES(idProvinciaBuenosAires,'Buenos Aires');
 
 CREATE TABLE localidad (
  idLocalidad INTEGER NOT NULL,
@@ -14,7 +15,7 @@ CREATE TABLE localidad (
  PRIMARY KEY(idLocalidad),
  FOREIGN KEY(idProvincia) REFERENCES provincia(idProvincia)
 );
-INSERT INTO "localidad" VALUES(0,'CABA',0);
+INSERT INTO "localidad" VALUES(idLocalidadCABA,'CABA',idProvinciaBuenosAires);
 
 CREATE TABLE calle (
  idCalle INTEGER NOT NULL,
@@ -23,9 +24,9 @@ CREATE TABLE calle (
  PRIMARY KEY(idCalle),
  FOREIGN KEY(idLocalidad) REFERENCES localidad(idLocalidad)
 );
-INSERT INTO "calle" VALUES(0,'Mendoza',0);
-INSERT INTO "calle" VALUES(1,'Artilleros',0);
-INSERT INTO "calle" VALUES(2,'Cabildo',0);
+INSERT INTO "calle" VALUES(idCalleMendoza,'Mendoza',idLocalidadCABA);
+INSERT INTO "calle" VALUES(idCalleArtilleros,'Artilleros',idLocalidadCABA);
+INSERT INTO "calle" VALUES(idCalleCabildo,'Cabildo',idLocalidadCABA);
 
 CREATE TABLE direccion (
  idDireccion INTEGER NOT NULL,
@@ -34,18 +35,18 @@ CREATE TABLE direccion (
  PRIMARY KEY(idDireccion),
  FOREIGN KEY(idCalle) REFERENCES calle(idCalle)
 );
-INSERT INTO "direccion" VALUES(0,1200,0);
-INSERT INTO "direccion" VALUES(1,2081,1);
-INSERT INTO "direccion" VALUES(2,4000,2);
+INSERT INTO "direccion" VALUES(idDireccionMendoza,1200,idCalleMendoza);
+INSERT INTO "direccion" VALUES(idDireccionArtilleros,2081,idCalleArtilleros);
+INSERT INTO "direccion" VALUES(idDireccionCabildo,4000,idCalleCabildo);
 
 CREATE TABLE tipo_de_lugar (
  idTipoLugar INTEGER NOT NULL,
  tipo VARCHAR(255) DEFAULT NULL,
  PRIMARY KEY(idTipoLugar)
 );
-INSERT INTO "tipo_de_lugar" VALUES(0,'Calle');
-INSERT INTO "tipo_de_lugar" VALUES(1,'Avenida');
-INSERT INTO "tipo_de_lugar" VALUES(2,'Autopista');
+INSERT INTO "tipo_de_lugar" VALUES(idTipoLugarCalle,'Calle');
+INSERT INTO "tipo_de_lugar" VALUES(idTipoLugarAvenida,'Avenida');
+INSERT INTO "tipo_de_lugar" VALUES(idTipoLugarAutopista,'Autopista');
 
 CREATE TABLE calle_tiene_tipo_de_lugar (
  idCalle INTEGER NOT NULL,
@@ -56,9 +57,9 @@ CREATE TABLE calle_tiene_tipo_de_lugar (
  FOREIGN KEY(idCalle) REFERENCES calle(idCalle),
  FOREIGN KEY(idTipoLugar) REFERENCES tipo_de_lugar(idTipoLugar)
 );
-INSERT INTO "calle_tiene_tipo_de_lugar" VALUES(0,0,0,5000);
-INSERT INTO "calle_tiene_tipo_de_lugar" VALUES(1,0,0,1000);
-INSERT INTO "calle_tiene_tipo_de_lugar" VALUES(2,1,0,10000);
+INSERT INTO "calle_tiene_tipo_de_lugar" VALUES(idCalleMendoza,idTipoLugarCalle,0,longitudMendoza);
+INSERT INTO "calle_tiene_tipo_de_lugar" VALUES(idCalleArtilleros,idTipoLugarCalle,0,longitudArtilleros);
+INSERT INTO "calle_tiene_tipo_de_lugar" VALUES(idCalleCabildo,idTipoLugarAvenida,0,longitudCabildo);
 
 CREATE TABLE comisaria (
  nroComisaria INTEGER NOT NULL,
@@ -67,7 +68,7 @@ CREATE TABLE comisaria (
  PRIMARY KEY(nroComisaria),
  FOREIGN KEY(idDireccion) REFERENCES direccion(idDireccion)
 );
-INSERT INTO "comisaria" VALUES(51,'Comisaria 51',1);
+INSERT INTO "comisaria" VALUES(nroComisariaComisaria51,'Comisaria 51',idDireccionArtilleros);
 
 CREATE TABLE denuncia (
  nroDenuncia INTEGER NOT NULL,
@@ -76,21 +77,21 @@ CREATE TABLE denuncia (
  PRIMARY KEY(nroDenuncia),
  FOREIGN KEY(nroComisaria) REFERENCES comisaria(nroComisaria)
 );
-INSERT INTO "denuncia" VALUES(0,'El chano rompio todo.',51);
+INSERT INTO "denuncia" VALUES(nroDenunciaChano,'El chano rompio todo.',nroComisariaComisaria51);
 
 CREATE TABLE tipo_de_colision (
  idTipoColision INTEGER NOT NULL,
  tipo VARCHAR(255) DEFAULT NULL,
  PRIMARY KEY(idTipoColision)
 );
-INSERT INTO "tipo_de_colision" VALUES(0,'Vehicular');
+INSERT INTO "tipo_de_colision" VALUES(idTipoColisionVehicular,'Vehicular');
 
 CREATE TABLE modalidad (
  idTipoModalidad INTEGER NOT NULL,
  tipo VARCHAR(255) DEFAULT NULL,
  PRIMARY KEY(idTipoModalidad)
 );
-INSERT INTO "modalidad" VALUES(0,'Choque');
+INSERT INTO "modalidad" VALUES(idTipoModalidadChoque,'Choque');
 
 CREATE TABLE siniestro (
  idSiniestro INTEGER NOT NULL,
@@ -101,7 +102,7 @@ CREATE TABLE siniestro (
  FOREIGN KEY(nroDenuncia) REFERENCES denuncia(nroDenuncia),
  FOREIGN KEY(idDireccion) REFERENCES direccion(idDireccion)
 );
-INSERT INTO "siniestro" VALUES(0,'05/08/2015',0,0);
+INSERT INTO "siniestro" VALUES(idSiniestroChano,'05/08/2015',nroDenunciaChano,idDireccionMendoza);
 
 CREATE TABLE siniestro_damnifica_tipo_de_colision (
  idSiniestro INTEGER NOT NULL,
@@ -110,7 +111,7 @@ CREATE TABLE siniestro_damnifica_tipo_de_colision (
  FOREIGN KEY(idSiniestro) REFERENCES siniestro(idSiniestro),
  FOREIGN KEY(idTipoColision) REFERENCES tipo_de_colision(idTipoColision)
 );
-INSERT INTO "siniestro_damnifica_tipo_de_colision" VALUES(0,0);
+INSERT INTO "siniestro_damnifica_tipo_de_colision" VALUES(idSiniestroChano,idTipoColisionVehicular);
 
 CREATE TABLE siniestro_forma_de_modalidad (
  idSiniestro INTEGER NOT NULL,
@@ -119,14 +120,14 @@ CREATE TABLE siniestro_forma_de_modalidad (
  FOREIGN KEY(idSiniestro) REFERENCES siniestro(idSiniestro),
  FOREIGN KEY(idTipoModalidad) REFERENCES modalidad(idTipoModalidad)
 );
-INSERT INTO "siniestro_forma_de_modalidad" VALUES(0,0);
+INSERT INTO "siniestro_forma_de_modalidad" VALUES(idSiniestroChano,idTipoPavimentoNormal);
 
 CREATE TABLE pavimento (
  idPavimento INTEGER NOT NULL,
  descripcion VARCHAR(255) DEFAULT NULL,
  PRIMARY KEY(idPavimento)
 );
-INSERT INTO "pavimento" VALUES(0,'pavimento normal');
+INSERT INTO "pavimento" VALUES(idTipoPavimentoNormal,'pavimento normal');
 
 CREATE TABLE estudio (
  idEstudio INTEGER NOT NULL,
@@ -140,7 +141,7 @@ CREATE TABLE estudio (
  FOREIGN KEY(idTipoPavimento) REFERENCES pavimento(idTipoPavimento),
  FOREIGN KEY(idSiniestro) REFERENCES siniestro(idSiniestro)
 );
-INSERT INTO "estudio" VALUES(0,'El Chano venia pisteando como un campeon en contramano y choco 6 autos.','Buena condicion de via.','De noche, pero buena iluminacion.',0,0,0);
+INSERT INTO "estudio" VALUES(idEstudioChano,'El Chano venia pisteando como un campeon en contramano y choco 6 autos.','Buena condicion de via.','De noche, pero buena iluminacion.',False,idTipoPavimentoNormal,idSiniestroChano);
 
 CREATE TABLE persona (
  dni INTEGER NOT NULL,
@@ -149,9 +150,9 @@ CREATE TABLE persona (
  nacimiento DATE NOT NULL,
  PRIMARY KEY(dni)
 );
-INSERT INTO "persona" VALUES(15151515,'Ana','Arias','01/01/1971');
-INSERT INTO "persona" VALUES(20202020,'Tomas','Troglio','07/07/1977');
-INSERT INTO "persona" VALUES(28282828,'Santiago','Moreno Charpentier','23/09/1981');
+INSERT INTO "persona" VALUES(dniAna,'Ana','Arias','01/01/1971');
+INSERT INTO "persona" VALUES(dniTestigo,'Tomas','Troglio','07/07/1977');
+INSERT INTO "persona" VALUES(dniChano,'Santiago','Moreno Charpentier','23/09/1981');
 
 CREATE TABLE siniestro_testigo_persona (
  idSiniestro INTEGER NOT NULL,
@@ -160,9 +161,9 @@ CREATE TABLE siniestro_testigo_persona (
  FOREIGN KEY(idSiniestro) REFERENCES siniestro(idSiniestro),
  FOREIGN KEY(dni) REFERENCES persona(dni)
 );
-INSERT INTO "siniestro_testigo_persona" VALUES(0,20202020);
+INSERT INTO "siniestro_testigo_persona" VALUES(idEstudioChano,dniTestigo);
 
-CREATE TABLE cinturon (
+CREATE TABLE estudio_cinturon_persona (
  idEstudio INTEGER NOT NULL,
  dni INTEGER NOT NULL,
  si_o_no BOOLEAN NOT NULL,
@@ -170,14 +171,14 @@ CREATE TABLE cinturon (
  FOREIGN KEY(idEstudio) REFERENCES estudio(idEstudio),
  FOREIGN KEY(dni) REFERENCES persona(dni)
 );
-INSERT INTO "cinturon" VALUES(0,28282828,0);
+INSERT INTO "estudio_cinturon_persona" VALUES(idEstudioChano,dniChano,False);
 
 CREATE TABLE tipo_de_delito (
  idTipoDelito INTEGER NOT NULL,
  descripcion VARCHAR(255) DEFAULT NULL,
  PRIMARY KEY(idTipoDelito)
 );
-INSERT INTO "tipo_de_delito" VALUES(0,'Consumo Ilegal de Drogas');
+INSERT INTO "tipo_de_delito" VALUES(idTipoDelitoDrogas,'Consumo Ilegal de Drogas');
 
 CREATE TABLE antecedente_penal (
  idAntecedente INTEGER NOT NULL,
@@ -188,14 +189,14 @@ CREATE TABLE antecedente_penal (
  FOREIGN KEY(idTipoDelito) REFERENCES tipo_de_delito(idTipoDelito),
  PRIMARY KEY(idAntecedente)
 );
-INSERT INTO "antecedente_penal" VALUES(0,'06/06/2015',28282828,0);
+INSERT INTO "antecedente_penal" VALUES(idAntecedenteChano,'06/06/2015',dniChano,idTipoDelitoDrogas);
 
 CREATE TABLE tipo_de_infraccion (
  idTipoInfraccion INTEGER NOT NULL,
  descripcion VARCHAR(255) DEFAULT NULL,
  PRIMARY KEY(idTipoInfraccion)
 );
-INSERT INTO "tipo_de_infraccion" VALUES(0,'Exceso de velocidad permitida.');
+INSERT INTO "tipo_de_infraccion" VALUES(idTipoInfraccionExcesoVelocidad,'Exceso de velocidad permitida.');
 
 CREATE TABLE infraccion_de_transito (
  idInfraccion INTEGER NOT NULL,
@@ -205,14 +206,14 @@ CREATE TABLE infraccion_de_transito (
  FOREIGN KEY(idTipoInfraccion) REFERENCES tipo_infraccion(idTipoInfraccion),
  PRIMARY KEY(idInfraccion)
 );
-INSERT INTO "infraccion_de_transito" VALUES(0,2,0);
+INSERT INTO "infraccion_de_transito" VALUES(idInfraccionChano,idDireccionCabildo,idTipoInfraccionExcesoVelocidad);
 
 CREATE TABLE persona_con_licencia (
  dni INTEGER NOT NULL,
  FOREIGN KEY(dni) REFERENCES persona(dni),
  PRIMARY KEY(dni)
 );
-INSERT INTO "persona_con_licencia" VALUES(28282828);
+INSERT INTO "persona_con_licencia" VALUES(dniChano);
 
 CREATE TABLE licencia (
  nroLicencia INTEGER NOT NULL,
@@ -222,37 +223,37 @@ CREATE TABLE licencia (
  FOREIGN KEY(dni) REFERENCES persona_con_licencia(dni),
  PRIMARY KEY (nroLicencia, dni)
 );
-INSERT INTO "licencia" VALUES(0,28282828,'01/01/2015','01/01/2016');
+INSERT INTO "licencia" VALUES(nroLicenciaChano,dniChano,'01/01/2015','01/01/2016');
 
 CREATE TABLE compania_de_seguro (
  cuit INTEGER NOT NULL,
  nombre VARCHAR(255) NOT NULL,
  PRIMARY KEY(cuit)
 );
-INSERT INTO "compania_de_seguro" VALUES(30663205621,'La Caja');
+INSERT INTO "compania_de_seguro" VALUES(cuitLaCaja,'La Caja');
 
 CREATE TABLE tipo_de_cobertura (
  idTipoCobertura INTEGER NOT NULL,
  descripcion VARCHAR(255) NOT NULL,
  PRIMARY KEY(idTipoCobertura)
 );
-INSERT INTO "tipo_de_cobertura" VALUES(0,'cobertura total.');
+INSERT INTO "tipo_de_cobertura" VALUES(idTipoCoberturaTotal,'cobertura total.');
 
 CREATE TABLE tipo_de_vehiculo (
  idTipoVehiculo INTEGER NOT NULL,
  descripcion VARCHAR(255) NOT NULL,
  PRIMARY KEY(idTipoVehiculo)
 );
-INSERT INTO "tipo_de_vehiculo" VALUES(0,'Auto');
-INSERT INTO "tipo_de_vehiculo" VALUES(1,'Camioneta');
+INSERT INTO "tipo_de_vehiculo" VALUES(idTipoVehiculoAuto,'Auto');
+INSERT INTO "tipo_de_vehiculo" VALUES(idTipoVehiculoCamioneta,'Camioneta');
 
 CREATE TABLE categoria_de_vehiculo (
  idCategoria INTEGER NOT NULL,
  descripcion VARCHAR(255) NOT NULL,
  PRIMARY KEY(idCategoria)
 );
-INSERT INTO "categoria_de_vehiculo" VALUES(0,'Gama media');
-INSERT INTO "categoria_de_vehiculo" VALUES(1,'Gama alta');
+INSERT INTO "categoria_de_vehiculo" VALUES(idCategoriaGamaMedia,'Gama media');
+INSERT INTO "categoria_de_vehiculo" VALUES(idCategoriaGamaAlta,'Gama alta');
 
 CREATE TABLE vehiculo (
  nroPatente CHARACTER(6) NOT NULL,
@@ -265,8 +266,8 @@ CREATE TABLE vehiculo (
  FOREIGN KEY(dni) REFERENCES persona(dni),
  PRIMARY KEY(nroPatente)
 );
-INSERT INTO "vehiculo" VALUES("CHA123",'01/01/2010',1,1,28282828);
-INSERT INTO "vehiculo" VALUES("ANA123",'01/01/2009',0,0,15151515);
+INSERT INTO "vehiculo" VALUES(nroPatenteChano,'01/01/2010',idCategoriaGamaAlta,idTipoVehiculoCamioneta,dniChano);
+INSERT INTO "vehiculo" VALUES(nroPatenteAna,'01/01/2009',idCategoriaGamaMedia,idTipoVehiculoAuto,dniAna);
 
 CREATE TABLE seguro (
  idSeguro INTEGER NOT NULL,
@@ -278,7 +279,7 @@ CREATE TABLE seguro (
  FOREIGN KEY(nroPatente) REFERENCES vehiculo(nroPatente),
  PRIMARY KEY(idSeguro)
 );
-INSERT INTO "seguro" VALUES(0,30663205621,0,"ANA123");
+INSERT INTO "seguro" VALUES(idSeguroAna,cuitLaCaja,idTipoCoberturaTotal,nroPatenteAna);
 
 CREATE TABLE cedula (
  nroPatente CHARACTER(6) NOT NULL,
@@ -287,17 +288,17 @@ CREATE TABLE cedula (
  FOREIGN KEY(dni) REFERENCES persona_con_licencia(dni),
  PRIMARY KEY(nroPatente, dni)
 );
-INSERT INTO "cedula" VALUES("CHA123",28282828);
-INSERT INTO "cedula" VALUES("ANA123",15151515);
+INSERT INTO "cedula" VALUES(nroPatenteChano,dniChano);
+INSERT INTO "cedula" VALUES(nroPatenteAna,dniAna);
 
-CREATE TABLE protagoniza (
+CREATE TABLE siniestro_protagoniza_vehiculo (
  nroPatente CHARACTER(6) NOT NULL,
  idSiniestro INTEGER NOT NULL,
  FOREIGN KEY(nroPatente) REFERENCES vehiculo(nroPatente),
  FOREIGN KEY(idSiniestro) REFERENCES siniestro(idSiniestro),
  PRIMARY KEY(nroPatente, idSiniestro)
 );
-INSERT INTO "protagoniza" VALUES("ANA123",0);
+INSERT INTO "siniestro_protagoniza_vehiculo" VALUES(nroPatenteAna,idSiniestroChano);
 
 CREATE TABLE accidente (
  nroPatente CHARACTER(6) NOT NULL,
@@ -308,16 +309,16 @@ CREATE TABLE accidente (
  FOREIGN KEY(dni) REFERENCES persona(dni), 
  PRIMARY KEY(nroPatente, idSiniestro)
 );
-INSERT INTO "accidente" VALUES("CHA123",0,28282828);
+INSERT INTO "accidente" VALUES(nroPatenteChano,idSiniestroChano,dniChano);
 
-CREATE TABLE culpable (
+CREATE TABLE siniestro_vehiculo_persona (
  idSiniestro INTEGER NOT NULL,
  dni INETGER NOT NULL,
  FOREIGN KEY(idSiniestro) REFERENCES siniestro(idSiniestro)
  FOREIGN KEY(dni) REFERENCES persona(dni),
  PRIMARY KEY(idSiniestro,dni)
 );
-INSERT INTO "culpable" VALUES(0,28282828);
+INSERT INTO "siniestro_vehiculo_persona" VALUES(idSiniestroChano,dniChano);
 
 CREATE TABLE persona_en_vehiculo_comete_infraccion (
  nroPatente CHARACTER(6) NOT NULL,
@@ -328,6 +329,6 @@ CREATE TABLE persona_en_vehiculo_comete_infraccion (
  FOREIGN KEY(idInfraccion) REFERENCES infraccion_de_transito(idInfraccion), 
  PRIMARY KEY(nroPatente, dni, idInfraccion)
 );
-INSERT INTO "persona_en_vehiculo_comete_infraccion" VALUES("CHA123",28282828,0);
+INSERT INTO "persona_en_vehiculo_comete_infraccion" VALUES(nroPatenteChano,dniChano,idInfraccionChano);
 
 COMMIT;
